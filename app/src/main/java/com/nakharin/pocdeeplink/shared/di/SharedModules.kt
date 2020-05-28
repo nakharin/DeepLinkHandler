4c6920d0337b69c2d073cc9fafcf46cc4b8ce3f5
Nakharin <nakharin.s@linecorp.com>
2020-05-28 23:49:13 +0700

    - Revamp module #2

--- app/src/main/java/com/nakharin/pocdeeplink/shared/di/SharedModules.kt
+++ app/src/main/java/com/nakharin/pocdeeplink/shared/di/SharedModules.kt
@@ -2,14 +2,11 @@ package com.nakharin.pocdeeplink.shared.di
 
 import com.nakharin.pocdeeplink.shared.deeplink.di.DeeplinkModule
 import com.nakharin.pocdeeplink.shared.navigation.di.NavigationModule
-import org.koin.dsl.module
 
 object SharedModules {
 
-    val modules = module {
-        arrayListOf(
-            DeeplinkModule.module,
-            NavigationModule.module
-        )
-    }
+    val modules = listOf(
+        DeeplinkModule.module,
+        NavigationModule.module
+    )
 }
