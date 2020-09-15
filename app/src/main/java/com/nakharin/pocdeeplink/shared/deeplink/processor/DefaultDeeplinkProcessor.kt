daa9e884396074d5d12a55fb114aaed882a5d335
Nakharin <nakharin.s@linecorp.com>
2020-09-15 18:06:19 +0700

    - Added link

--- app/src/main/java/com/nakharin/pocdeeplink/shared/deeplink/processor/DefaultDeeplinkProcessor.kt
+++ app/src/main/java/com/nakharin/pocdeeplink/shared/deeplink/processor/DefaultDeeplinkProcessor.kt
@@ -3,6 +3,7 @@ package com.nakharin.pocdeeplink.shared.deeplink.processor
 import android.net.Uri
 import com.nakharin.pocdeeplink.shared.deeplink.command.DeeplinkCommand
 
+// https://blog.usejournal.com/navigation-in-modular-applications-with-deep-linking-6a599c11e487
 class DefaultDeeplinkProcessor(
     private val commands: Set<@JvmSuppressWildcards DeeplinkCommand>
 ) : DeeplinkProcessor {
